2b0855be943a21f623194a14089da2ebc6c0ca4b
HlaingMinThein <hlaingmin1235145@gmail.com>
2020-01-14 13:55:51 +0630

    blabla

--- src/features/app/Footer.js
+++ src/features/app/Footer.js
@@ -24,14 +24,9 @@ const Footer = props => {
             <div className="col-lg-3 col-md-3 col-12 pb-4">
               <img src={topmyanmar} className="w-75" alt="" />
             </div>
-            <div className="col-lg-4 col-md-4 col-12 px-2 text-left">
-              <h5>Don't know where to start? Let's get in touch!</h5>
-              <div>
-                Let us know what we can do to make your life easier and feel confident that your loved one is getting the care they deserve.
-              </div>
-            </div>
+            
             <div className="col-lg-5 col-md-5 col-12 px-2 text-left">
-              <h5>HOW TO FIND US</h5>
+            <h5>HOW TO FIND US</h5>
               <div style={{ marginBottom: '12px' }}><i className="fa fa-map-marker for-icon" aria-hidden="true"></i>
                 PaPa 3/B(Kha), Yaw Minn Gyi Road, 68th street, Industrial Zone (1), Pyigitagon Township, Mandalay
             </div>
@@ -41,6 +36,23 @@ const Footer = props => {
               <div style={{ marginBottom: '12px' }}><i className="fa fa-envelope-o for-icon" aria-hidden="true"></i>
                 topmyanmar.mtn@gmail.com
             </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-12 px-2 text-left">
+              
+            {/* <div className="align-self-center">
+                  <a alt="" href="www.facebook.com" style={{ color: '#fcff23', fontSize: '35px' }}>
+                    <i className="fa fa-facebook pr-3" />
+                  </a>
+                  <a href="www.twitter.com" style={{ color: '#fcff23', fontSize: '35px'  }}>
+                    <i className="fa fa-twitter pr-3" />
+                  </a>
+                  <a href="www.instagram.com" style={{ color: '#fcff23', fontSize: '35px'  }}>
+                    <i className="fa fa-instagram pr-3" />
+                  </a>
+                  <a href="www.youtube.com" style={{ color: '#fcff23', fontSize: '35px'  }}>
+                    <i className="fa fa-youtube pr-3" />
+                  </a>
+                </div>  */}
             </div>
           </div>
         </div>
